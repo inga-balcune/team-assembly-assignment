447ecc2425bdea78a570af43b9a5312ddde20ffc
Inga Balcune <inga.balcune@gmail.com>
2019-09-04 19:20:04 +0200

    added polyfills for IE

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,9 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
+    script: [
+      { src: `https://polyfill.io/v3/polyfill.min.js?features=es7%2CSet%2CRegExp.prototype.flags%2CPromise.prototype.finally%2CPromise%2CObject.values%2CObject.setPrototypeOf%2CObject.seal%2CObject.preventExtensions%2CObject.keys%2CObject.isSealed%2CObject.entries%2CArray.prototype.find%2Cdefault%2Ces2015%2Ces2016%2Ces2017%2Ces5%2Ces6%2CArray.from%2CArray.isArray%2CArray.of%2CArray.prototype.copyWithin%2CArray.prototype.entries%2CArray.prototype.every%2CArray.prototype.fill%2CArray.prototype.filter%2CArray.prototype.findIndex%2CArray.prototype.flat%2CArray.prototype.flatMap%2CArray.prototype.forEach%2CArray.prototype.includes%2CArray.prototype.indexOf%2CArray.prototype.keys%2CArray.prototype.lastIndexOf%2CArray.prototype.map%2CArray.prototype.reduce%2CArray.prototype.reduceRight%2CArray.prototype.some%2CArray.prototype.values`, body: true },
+  ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       { rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=Material+Icons' },
