0400e56215dc4ad586abc73f654ce3b7df66ddc7
Inga Balcune <inga.balcune@gmail.com>
2019-09-07 18:36:30 +0200

    added error page

---
+++ layouts/error.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <md-empty-state
+      md-icon="block"
+      md-label="Sorry"
+      md-description="Seems like something went wrong.">
+      <md-button to="/admin" class="md-primary md-raised">Back to safety</md-button>
+    </md-empty-state>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'admin'
+}
+</script>
+
+<style lang='scss' scoped>
+
+div {
+    text-align: center;
+  }
+
+  .md-empty-state {
+    display: inline-block;
+    vertical-align: middle;
+
+    + .md-empty-state {
+      margin-left: 16px;
+    }
+  }
+
+</style>
--- pages/admin/client/_clientId/index.vue
+++ pages/admin/client/_clientId/index.vue
@@ -28,7 +28,6 @@ export default {
   }
 }
 </script>
-
 <style lang="scss">
   
 </style>
