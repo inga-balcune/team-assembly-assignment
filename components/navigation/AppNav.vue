55d6a642e56b93bc1f02eef7bafdabfe0c168cc9
Inga Balcune <ibal@siteimprove.com>
2020-04-12 12:37:07 +0200

    Update portfolio url

--- components/navigation/AppNav.vue
+++ components/navigation/AppNav.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <md-toolbar md-elevation="0" class="md-primary">
-        <a style="flex: 1" href="https://inga-balcune-portfolio.herokuapp.com/" target='blank'><h1 class="md-title">Signifly Assignment <span class='subtitle'>by Inga Balcune</span></h1></a>
-        <md-button to='/admin' >Dashboard</md-button>
+      <a style="flex: 1" href="https://ingabalcune.dev/" target="blank">
+        <h1 class="md-title">
+          Signifly Assignment
+          <span class="subtitle">by Inga Balcune</span>
+        </h1>
+      </a>
+      <md-button to="/admin">Dashboard</md-button>
     </md-toolbar>
   </div>
 </template>
 
 <style lang='scss' scoped>
-
 a {
   &:hover {
     text-decoration: none;
@@ -17,7 +21,7 @@ a {
 
 .subtitle {
   display: block;
-  margin-top: -.8rem;
+  margin-top: -0.8rem;
   font-size: 1.6rem;
   font-weight: 700;
 
@@ -30,5 +34,4 @@ a {
   background-color: $color-signifly-dark-blue;
   margin-bottom: 2rem;
 }
-
 </style>
